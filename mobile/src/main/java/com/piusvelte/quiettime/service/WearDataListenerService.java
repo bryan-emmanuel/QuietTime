3184f2d31cb0252724ab6ed94dc86316f1a221ce
Bryan Emmanuel <bemmanuel@meetme.com>
2014-11-25 20:48:39 -0500

    Release 2.1
    Ignore ringer mode check since ringer mode no longer changes on
    Android 5.0 Lollipop
    
    Update icons

--- mobile/src/main/java/com/piusvelte/quiettime/service/WearDataListenerService.java
+++ mobile/src/main/java/com/piusvelte/quiettime/service/WearDataListenerService.java
@@ -67,19 +67,17 @@ public class WearDataListenerService extends WearableListenerService {
                 boolean inZenMode = dataMap.getBoolean(DataHelper.KEY_IN_ZEN_MODE);
                 AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-                if (BuildConfig.DEBUG) Log.d(TAG, "onDataChanged, inZenMode? " + inZenMode);
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "onDataChanged, inZenMode? " + inZenMode + ", ringer mode: " + audioManager.getRingerMode());
+                }
 
                 if (inZenMode) {
                     if (PreferencesHelper.isMutePhoneEnabled(sharedPreferences)) {
                         int muteMode = PreferencesHelper.getMutePhoneMode(sharedPreferences);
-
-                        if (muteMode != audioManager.getRingerMode()) {
-                            alertRingerChange(sharedPreferences);
-                            audioManager.setRingerMode(muteMode);
-                        }
+                        alertRingerChange(sharedPreferences);
+                        audioManager.setRingerMode(muteMode);
                     }
-                } else if (PreferencesHelper.isUnmutePhoneEnabled(sharedPreferences)
-                        && audioManager.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+                } else if (PreferencesHelper.isUnmutePhoneEnabled(sharedPreferences)) {
                     alertRingerChange(sharedPreferences);
                     audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 }
